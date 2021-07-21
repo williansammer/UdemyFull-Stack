30190fe8bd07575420088d5001de5e4e1531d321
Willian Sammer Gabriel <willian.sgabriel05@gmail.com>
2021-07-21 20:25:46 -0300

    Angular
    Bootstrap
    font awesome
    Usando Deretivas
    Configurando Cors
    Dando get na API dos itens

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [		
+  declarations: [
     AppComponent,
       EventosComponent,
       PalestrantesComponent
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public eventos : any;
 
   ngOnInit() {
+    this.getEventos();
+  }
+
+  public getEventos():void{
+    this.http.get(`https://localhost:6001/api/eventos`).subscribe(
+      response => this.eventos = response,
+      error => console.error(error)
+    )
   }
 
 }
